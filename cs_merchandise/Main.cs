850ceaee3747779d5b7fa445dac4544ad1694b74
RJ Perias <rjperias0213@gmail.com>
2018-06-06 20:07:01 +0800

    Rj (#8)
    * partial changes to query
    
    * used master ui code
    
    * finished my shit, fixed queries & created for order edit
    
    * Small patch to remove debug code
    
    * patch for databaseConn

--- cs_merchandise/Main.cs
+++ cs_merchandise/Main.cs
@@ -63,11 +63,12 @@ namespace cs_merchandise
             this.payment = payment;
             this.change = change;
 
-            conn.Insert("orders", "order_date", DateTime.Now.ToString("yyyy-MM-dd")
-                /*order_date.ToString("dd/MM/yyyy")*/, "order_status", "1", "customer_id", customer_id.ToString(), "payment_status", paystatus, "payment", payment.ToString()).GetQueryData();
+            conn.Insert("orders", "order_date", DateTime.Now.ToString("yyyy-MM-dd"), "order_status", "1", "customer_id", customer_id.ToString(), "payment_status", paystatus).GetQueryData();
+            conn.Insert("order_payment", "order_id", conn.lastID, "payment", payment.ToString(), "payment_date",
+                DateTime.Now.ToString("yyyy-MM-dd")).GetQueryData();
             //orderline.ColumnCount = 5;
             string temp_orderid;
-            temp_orderid = conn.lastID();
+            temp_orderid = conn.lastID;
             foreach (DataGridViewRow rows in orderline.Rows)
             {
                 MessageBox.Show(rows.Cells[0].Value.ToString());
@@ -421,15 +422,15 @@ namespace cs_merchandise
 
         private void remove_merch_Click(object sender, EventArgs e)
         {
-            if(orderline.Rows.Count > 0)
+            if (orderline.Rows.Count > 0)
             {
                 orderline.Rows.RemoveAt(orderline.CurrentRow.Index);
             }
             else
             {
                 MessageBox.Show("No Item in Orderline");
-
             }
+
             price_total.Text = (from DataGridViewRow row in orderline.Rows
                                 where row.Cells[2].FormattedValue.ToString() != string.Empty
                                 select Convert.ToDecimal(row.Cells[2].FormattedValue)).Sum().ToString();
@@ -528,17 +529,21 @@ namespace cs_merchandise
 
         private void reloadOrders()
         {
-            dataGridView2.DataSource = conn.Select("orders", "orders.order_id", "orders.order_date", "orders.order_status", "CONCAT(customer.lastname, ', ', customer.firstname) as customer")
-                .IJoin("customer", "orders.customer_id", "customer.customer_id")
+            dataGridView2.DataSource = conn.Select("orders=o", "o.order_id", "op.payment_date AS 'Order Date'", "o.order_status", "CONCAT(c.lastname, ', ', c.firstname) as customer")
+                .NJoin("order_payment=op")
+                .NJoin("customer=c")
+                .Group("order_id")
                 .GetQueryData();
         }
 
         private void showOrderDetails()
         {
             string selectedOrder = dataGridView2.SelectedRows[0].Cells[0].Value.ToString();
-            var customerDetails = conn.Select("orders=o", "CONCAT(c.firstname,' ',c.lastname) as name", "c.contact", "c.cluster", "o.order_date", "o.payment_status", "o.payment", "o.order_status ")
+            var customerDetails = conn.Select("orders=o", "CONCAT(c.firstname,' ',c.lastname) as name", "c.contact", "c.cluster", "op.payment_date", "o.payment_status", "SUM(op.payment)", "o.order_status ")
                                     .NJoin("customer=c")
+                                    .NJoin("order_payment=op")
                                     .Where("o.order_id", selectedOrder)
+                                    .Group("order_id")
                                     .GetQueryData();
             orderCname.Text = customerDetails.Rows[0][0].ToString();
             orderCcontact.Text = customerDetails.Rows[0][1].ToString();
@@ -547,35 +552,53 @@ namespace cs_merchandise
             orderOpstatus.Text = customerDetails.Rows[0][4].ToString();
             orderOcdate.Text = customerDetails.Rows[0][5].ToString();
             orderOstatus.Text = customerDetails.Rows[0][6].ToString();
-
-            dataGridView3.DataSource = conn.Select("orderline=ol", "m.merch_name", "ol.quantity", "ol.total_price")
+            dataGridView3.DataSource = conn.Select("orderline=ol", "m.merch_name", "ol.quantity", "ol.quantity_claimed")
                                         .NJoin("merchandise=m")
                                         .NJoin("orders=o")
                                         .Where("o.order_id", selectedOrder)
                                         .GetQueryData();     
         }
 
-        public void claimOrder(string olID)
+        public void claimOrder(string olID, string quantity)
         {
-            conn.Update("orderline", "claim_date" , DateTime.Now.ToString("yyyy-MM-dd"))
+            var quantityClaimed = conn.Select("orderline", "quantity_claimed")
                 .Where("orderlineID", olID)
                 .GetQueryData();
-            
+            quantityClaimed += int.Parse(quantity);
+            conn.Update("orderline", "quantity_claimed", quantityClaimed)
+                .Where("orderline_id", olID)
+                .GetQueryData();
+            conn.Insert("order_claim", "orderline_id", olID, "quantity_no", quantity, "date_claimed",
+                    DateTime.Now.ToString("yyyy-MM-dd"))
+                .GetQueryData();
+
         }
 
-        public void payOrder(string oID, string payment)
-        {
-            payOrder(oID, Convert.ToInt32(payment));
-        }
+        public void payOrder(string oID, string payment) => payOrder(oID, Convert.ToInt32(payment));
 
         public void payOrder(string oID, int payment)
         {
-            string present_pay = conn.Select("orders", "payment")
+            // toDo: 1 insert for payment
+            var present_pay = conn.Select("order_payment", "SUM(payment)")
+                                    .NJoin("orders")
                                     .Where("order_id", oID)
-                                    .GetQueryData()[0][0];
+                                    .Group("order_id")
+                                    .GetQueryData()
+                                    .Rows[0][0];
             int total_pay =  Convert.ToInt32(present_pay) + payment;
-            conn.Update("orders", "payment", total_pay.ToString())
+            MessageBox.Show(total_pay.ToString());
+            conn.Insert("order_payment", "order_id", oID, "payment", payment.ToString(), "payment_date",
+                DateTime.Now.ToString("yyyy-MM-dd"))
                 .GetQueryData();
+            var total_price = conn.Select("orderline", "SUM(total_price)")
+                .Where("order_id", oID)
+                .Group("order_id")
+                .GetQueryData()
+                .Rows[0][0];
+            if(total_pay >= total_price)
+                conn.Update("orders", "payment_status", "1")
+                    .Where("order_id",oID)
+                    .GetQueryData();
         }
 
         private void dataGridView2_SelectionChanged(object sender, EventArgs e)
